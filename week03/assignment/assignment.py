f43bb15dda0b0ba0c3a99812a53501c1780152a5
Marcel Pratikto <marcelaap@byui.edu>
2022-10-04 01:01:58 -0600

    finished green screen assignment

--- week03/assignment/assignment.py
+++ week03/assignment/assignment.py
@@ -3,7 +3,7 @@
 Course: CSE 251
 Lesson Week: 03
 File: assignment.py
-Author: <Your Name>
+Author: Marcel Pratikto
 
 Purpose: Video Frame Processing
 
@@ -22,12 +22,14 @@ from PIL import Image
 import numpy as np
 import timeit
 import multiprocessing as mp
+import threading
 
 # Include cse 251 common Python files
 from cse251 import *
 
 # 4 more than the number of cpu's on your computer
-CPU_COUNT = mp.cpu_count() + 4  
+CPU_COUNT = mp.cpu_count() + 4 
+#CPU_COUNT = 4 
 
 # TODO Your final video need to have 300 processed frames.  However, while you are 
 # testing your code, set this much lower
@@ -42,7 +44,7 @@ def create_new_frame(image_file, green_file, process_file):
     """ Creates a new image file from image_file and green_file """
 
     # this print() statement is there to help see which frame is being processed
-    print(f'{process_file[-7:-4]}', end=',', flush=True)
+    #print(f'{process_file[-7:-4]}', end=',', flush=True)
 
     image_img = Image.open(image_file)
     green_img = Image.open(green_file)
@@ -61,10 +63,47 @@ def create_new_frame(image_file, green_file, process_file):
 
 
 # TODO add any functions to need here
+def separate_process(num_cores, num_img):
+  total_per_core = num_img // num_cores + 1
+  print("\n")
+  print(f"total_per_core: {total_per_core}")
+  r = []
+  tracker = 1
+  while tracker < num_img+1:
+    start = tracker
+    end = tracker + total_per_core
+    if end >= num_img:
+      end = num_img
+    r.append((start, end))
+    tracker = end + 1
+  print(f"r: {r}")
 
+  # p = []
+  # for i in range(len(r)):
+  #   p.append(mp.Process(target=merge_img_green, args=r[i]))
+  # for core in p:
+  #   core.start()
+  # for core in p:
+  #   core.join()
+  with mp.Pool(num_cores) as p: 
+    for i in range(len(r)):
+      merge_img_green(r[i][0], r[i][1])
+
+def merge_img_green(start, end):
+  p = []
+  for image_number in range(start, end+1):
+    image_file = rf'elephant/image{image_number:03d}.png'
+    green_file = rf'green/image{image_number:03d}.png'
+    process_file = rf'processed/image{image_number:03d}.png'
+    p.append(threading.Thread(target=create_new_frame, args=(image_file, green_file, process_file)))
+  for thread in p:
+    thread.start()
+  for thread in p:
+    thread.join()
 
 
 if __name__ == '__main__':
+    
     # single_file_processing(300)
     # print('cpu_count() =', cpu_count())
 
@@ -74,23 +113,34 @@ if __name__ == '__main__':
     xaxis_cpus = []
     yaxis_times = []
 
+    num_img = 0
+    for img in os.scandir("elephant"):
+      num_img += 1
+
     # TODO Process all frames trying 1 cpu, then 2, then 3, ... to CPU_COUNT
     #      add results to xaxis_cpus and yaxis_times
-
+    for i in range(1, CPU_COUNT+1):
+      start = timeit.default_timer() - all_process_time
+      separate_process(i, num_img)
+      end = timeit.default_timer() - all_process_time
+      total_time = end - start
+      #print(f"Time for {num_img} frames using {i} processes: {total_time}")
+      log.write(f"Time for {num_img} frames using {i} processes: {total_time}")
+      xaxis_cpus.append(i)
+      yaxis_times.append(end - start)
 
     # sample code: remove before submitting  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # process one frame #10
-    image_number = 10
+    # image_number = 10
 
-    image_file = rf'elephant/image{image_number:03d}.png'
-    green_file = rf'green/image{image_number:03d}.png'
-    process_file = rf'processed/image{image_number:03d}.png'
+    # image_file = rf'elephant/image{image_number:03d}.png'
+    # green_file = rf'green/image{image_number:03d}.png'
+    # process_file = rf'processed/image{image_number:03d}.png'
 
-    start_time = timeit.default_timer()
-    create_new_frame(image_file, green_file, process_file)
-    print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
+    # start_time = timeit.default_timer()
+    # create_new_frame(image_file, green_file, process_file)
+    # print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
 
     log.write(f'Total Time for ALL processing: {timeit.default_timer() - all_process_time}')
 
